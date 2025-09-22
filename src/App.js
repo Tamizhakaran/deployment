84b41817704669f9694e43a9d24220cc8fca98ba
Tamizhakaran <tamizhakaran10@gmail.com>
2025-09-22 13:39:02 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
      <div className="App">
       <h1>Hi, I'm Tamizhakaran. This is my first deployment of React Application</h1>
-      <h2>welcome to BIT!!</h2>
+      <h2>welcome to BIT College!!</h2>
     </div>
   );
 }
